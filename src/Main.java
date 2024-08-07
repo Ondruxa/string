e3a8a87842fa6d221b56e2b7851c2ab3130cff6c
Andrei Gorshkov <andruhinsans@mail.ru>
2024-08-07 22:11:31 +0300

    exam 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println("task1");
+        String firstName = "Ivanov";
+        String middleName = "Ivan";
+        String lastName = "Ivanovich";
+        String fullName = firstName + " " + middleName + " " + lastName;
+        System.out.println("Ф.И.О. сотрудника — " + fullName);
+        System.out.println("task2");
+        System.out.println("Данные Ф.И.О. сотрудника для заполнения отчета — " + fullName.toUpperCase());
+        System.out.println("task3");
+        fullName = "Иванов Семён Семёныч";
+        System.out.println("Данные Ф.И.О. сотрудника — " + fullName.replace("ё", "е"));
     }
 }
